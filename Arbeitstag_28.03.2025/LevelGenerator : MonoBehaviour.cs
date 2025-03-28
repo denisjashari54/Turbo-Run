f98ea56c9dddc433abb6015c375d837a34bb706b
Denis Jashari <107101355+denisjashari54@users.noreply.github.com>
2025-03-28 17:23:19 +0100

    Create LevelGenerator : MonoBehaviour.cs

---
+++ Arbeitstag_28.03.2025/LevelGenerator : MonoBehaviour.cs
@@ -0,0 +1,74 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    [SerializeField] GameObject chunkPrefab;
+    [SerializeField] int startingChunksAmount = 12;
+    [SerializeField] Transform chunkParent;
+    [SerializeField] float chunkLength = 10f;
+    [SerializeField] float moveSpeed = 8f;
+
+    List<GameObject> chunks = new List<GameObject>();  
+
+    void Start()
+    {
+        SpawnStartingChunks();  
+    }
+
+    void Update()
+    {
+        MoveChunk();
+    }
+
+    void SpawnStartingChunks()
+    {
+        for (int i = 0; i < startingChunksAmount; i++)
+        {
+            SpawnChunk();
+        }
+    }
+
+    void SpawnChunk()
+    {
+        float spawnPositionZ = CalculateSpawnPositionZ();
+
+        Vector3 chunkSpawnPos = new Vector3(transform.position.x, transform.position.y, spawnPositionZ);
+        GameObject newChunk = Instantiate(chunkPrefab, chunkSpawnPos, Quaternion.identity, chunkParent);
+
+        chunks.Add(newChunk);
+    }
+
+    float CalculateSpawnPositionZ()
+    {
+        float spawnPositionZ;
+
+        if (chunks.Count == 0)
+        {
+            spawnPositionZ = transform.position.z;
+        }
+        else
+        {
+            spawnPositionZ = chunks[chunks.Count - 1].transform.position.z + chunkLength;
+        }
+
+        return spawnPositionZ;
+    }
+
+    void MoveChunk()
+    {
+        for (int i = 0; i < chunks.Count; i++)
+        {
+            GameObject chunk = chunks[i];
+            chunk.transform.Translate(-transform.forward * (moveSpeed * Time.deltaTime));
+            
+            if (chunk.transform.position.z <= Camera.main.transform.position.z - chunkLength)
+            {
+                chunks.Remove(chunk);
+                Destroy(chunk);
+                SpawnChunk();
+            }
+        }
+    }
+}
